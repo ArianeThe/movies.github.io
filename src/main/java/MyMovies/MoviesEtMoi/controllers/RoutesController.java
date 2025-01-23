3b3da614555420846184ab24274a46ad1b2fb13b
Ariane Thebault <coppey.ariane@gmail.com>
2025-01-23 19:17:15 +0100

    MAJ name index instead home

--- src/main/java/MyMovies/MoviesEtMoi/controllers/RoutesController.java
+++ src/main/java/MyMovies/MoviesEtMoi/controllers/RoutesController.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class RoutesController {
         
     @GetMapping("/") 
-    public String home() { 
-        return "pages/home";  }
+    public String index() { 
+        return "pages/index";  }
     
 
     @GetMapping("/series") 
